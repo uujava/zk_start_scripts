6e61668f254f30679f8a58731d1fc072d8a94d0c
uujava <uujava@gmail.com>
2016-04-19 10:41:06 +0300

    clean version-2 directory on start

--- start_node.bat
+++ start_node.bat
@@ -7,6 +7,7 @@ set NODE_PATH=%NODE_BASE%%CFG_DIR%
 shift
 call %NODE_BASE%zkEnv.cmd
 @echo on
+del /F/Q %NODE_PATH%\data\version-2
 %JAVA_PATH% "-Dzookeeper.log.dir=%ZOO_LOG_DIR%" "-Dzookeeper.root.logger=%ZOO_LOG4J_PROP%" -cp "%ZOO_CLASSPATH%" %ZOOMAIN% "%ZOO_CFG%"
 endlocal
 
